dfd213a6762cec1d0aa520be5b27405ce2d14dd6
Sam Donald <sam@mudlabs.com.au>
2020-03-30 14:06:03 +1100

    Update comments

--- index.d.ts
+++ index.d.ts
@@ -1,15 +1,13 @@
 /**
  * Provides the English unit name for the given number.
- * - _(i.e. 10273 returns the string "thousand")_.
+ * - _(i.e. 17388 returns the string "thousand")_.
  */
 export function unitNameFromNumber(number:number): string;
-
 /**
  * Provides an abbreviation of the short unit name;
  * - _(i.e. "thousand" returns "K")_.
  */
 export function trimName(name:string): string;
-
 /**
  * Provides the base unit number for the given unit name.
  * - _(i.e. "thousand" returns the number 1000)_.
@@ -17,21 +15,21 @@ export function trimName(name:string): string;
 export function numberFromUnitName(name:string): number;
 
 /**
- * Provides the short-hand abbreviation of an number, relative to its unit.
- * - _(i.e. 10273 returns the number 10, because it is ten thousands)_.
+ * Provides the short-hand abbreviation of a number, relative to its unit.
+ * - _(i.e. 17388 returns the number 17, because it is 17 thousands)_.
  */
 export function trimNumber(number:number): number;
 
 /**
- * Provides the power of ten for the given number, relative to its unit.
- * - _(i.e. 10273 returns the power 3, because its unit is thousand)_.
+ * Provides the power for the given number, relative to its unit.
+ * - _(i.e. 17388 returns the power 3, because its unit is thousand)_.
  * - If you want its real power use `getPower`.
  */
 export function getUnitPower(number: number): number;
 
 /**
- * Provides the power of ten for the given number.
- * - _(i.e. 10273 returns the power 4)_.
+ * Provides the power for the given number.
+ * - _(i.e. 17388 returns the power 4)_.
  * - If you want the power relative the its unit use `getUnitPower`.
  */
 export function getPower(
